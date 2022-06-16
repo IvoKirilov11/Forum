979c18915566a3e335a566bc7f96461776ed87a7
IvoKirilov11 <mefistofel_1980@abv.bg>
2022-06-16 19:51:53 +0300

    AddLibmanFileAndChangeLayout

--- Web/Forum.Web.ViewModels/Categories/PostInCategoryViewModel.cs
+++ Web/Forum.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -7,7 +7,13 @@ namespace Forum.Web.ViewModels.Categories
     public class PostInCategoryViewModel : IMapFrom<Post>
     {
         public DateTime CreateOn { get; set; }
+
         public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortContent =>
+            this.Content?.Length > 50 ? this.Content?.Substring(0,50) + "..." : this.Content;
 
         public string UserUserName { get; set; }
 
